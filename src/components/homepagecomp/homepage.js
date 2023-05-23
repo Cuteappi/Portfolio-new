59a57cb17d2ae3cdd428f7bfae469680b1e4258a
solomon <you@example.com>
2023-05-24 02:36:33 +0530

    pageeeeee transitions

--- src/components/homepagecomp/homepage.js
+++ src/components/homepagecomp/homepage.js
@@ -1,34 +1,28 @@
+import styles from './scss/home.module.scss'
 import Navbar from '@/components/homepagecomp/Navbar'
 import AboutMe from '@/components/homepagecomp/about-me'
 import Skills from '@/components/homepagecomp/skills'
 import Projects from '@/components/homepagecomp/projects'
 import { HomeContext } from '@/contexts/HomeContext'
 import { useEffect, useRef, useContext } from 'react'
-import { gsap } from 'gsap'
 
 export default function Homepage() {
     const mainsec= useRef()
     const { setMainSec } = useContext(HomeContext)
+
     useEffect(() =>{
         setMainSec(mainsec)
-        
-        // router.beforePopState((state) => {
-        //     state.options.scroll = false;
-        //     return true;
-        // });
     },[])
 
     
     return (
-        <div>
-        <div style={{minHeight: '100vh', minWidth: '100%', background: 'rgb(255,85,85)', position: 'relative'}} ref={mainsec}>
+        <div className={`${styles.MainSec}`} ref={mainsec}>
             <Navbar />
             <AboutMe />
             <section style={{background:'black'}}>
                 <Skills />
                 <Projects />
             </section>
-        </div>
         </div>
     )
 }

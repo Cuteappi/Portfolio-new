60d5f663a719fb2c4779ea0b16f620ae0170088d
solomon <you@example.com>
2023-05-31 23:52:25 +0530

    made about me

---
+++ src/components/carasole/carasole.js
@@ -0,0 +1,156 @@
+import styles from './/carasole.module.scss'
+import { useEffect, useContext, useRef, useState } from 'react'
+//import { HomeContext } from '@/contexts/HomeContext'
+import { gsap }  from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function Carasole() {
+
+    //const { MainSec } = useContext(HomeContext)
+    const backstory = useRef()
+
+    useEffect(()=>{
+        const el = backstory.current.querySelector('.Backstory')
+        const Word = backstory.current.querySelector('.Word')
+        const Cross = backstory.current.querySelector('.Cross')
+        const Button = backstory.current.querySelectorAll('.Button')      
+        const Container = backstory.current.querySelector('.Container')
+
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+
+        Word.addEventListener('click', ()=>{
+            const ctx = gsap.context(()=>{
+                const x = gsap.timeline()
+                x.to(el,{x: 0, opacity: 1, duration: 0.5},0)
+                .to(el,{backdropFilter: 'blur(40px)', duration: 2},0.4)
+            })
+            return ()=> ctx.revert()
+        })
+
+        Cross.addEventListener('click', ()=>{
+            const ctx = gsap.context(()=>{
+                const x = gsap.timeline()
+                x.to(el,{backdropFilter: 'blur(10px)', duration: 2},0)
+                .to(el,{x: '100dvw', opacity: 0},0.2)
+            })
+            return ()=> ctx.revert()
+        })
+
+        Button[0].addEventListener('click', ()=>{
+            const ctx = gsap.context(()=>{
+                gsap.to(Container,{x: '0dvw', ease:'power4.out'})
+            })
+            return ()=> ctx.revert()
+        })
+        Button[1].addEventListener('click', ()=>{
+            const ctx = gsap.context(()=>{
+                gsap.to(Container,{x: '-100dvw', ease:'power4.out'})
+            })
+            return ()=> ctx.revert()
+        })
+
+        
+        Container.addEventListener('mousedown', (e) => {
+            isDown = true
+            startX = e.pageX - Container.offsetLeft
+            scrollLeft = Container.scrollLeft
+        })
+
+        Container.addEventListener('mouseleave', () => {
+            isDown = false
+        })
+
+        Container.addEventListener('mouseup', () => {
+            isDown = false
+        })
+
+        Container.addEventListener('mousemove', (e) => {
+            if(!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - Container.offsetLeft
+            const walk = (x - startX) * 3 //scroll-fast
+            Container.scrollLeft = scrollLeft - walk
+        })
+    })
+
+    return (
+        <div className={`${styles.about}`} ref={backstory}>
+            <button className={`${styles.about_word} Word`}>
+                ABOUT ME
+            </button>
+            
+            <div className={`${styles.about_Past} Backstory`}>
+                <div className={`${styles.about_Past_cross} Cross`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 32 32" className={`${styles.about_Past_cross_svg}`}>
+                        <path d="M18.8,16l5.5-5.5c0.8-0.8,0.8-2,0-2.8l0,0C24,7.3,23.5,7,23,7c-0.5,0-1,0.2-1.4,0.6L16,13.2l-5.5-5.5  c-0.8-0.8-2.1-0.8-2.8,0C7.3,8,7,8.5,7,9.1s0.2,1,0.6,1.4l5.5,5.5l-5.5,5.5C7.3,21.9,7,22.4,7,23c0,0.5,0.2,1,0.6,1.4  C8,24.8,8.5,25,9,25c0.5,0,1-0.2,1.4-0.6l5.5-5.5l5.5,5.5c0.8,0.8,2.1,0.8,2.8,0c0.8-0.8,0.8-2.1,0-2.8L18.8,16z"/>
+                    </svg>
+                </div>
+
+                <div className={`${styles.about_Past_section}`}>
+                    <div className={`${styles.about_Past_section_container} Container`}>
+                        <div className={`${styles.about_Past_section_me}`}>
+                            <div className={`${styles.about_Past_section_me_content}`}>
+
+                                <div className={`${styles.line}`}>
+                                    <h3 className={`${styles.h3}`}>Welcome to my portfolio page!</h3>
+                                </div>
+
+                                <p className={`${styles.p}`}>Here, you will discover a showcase of my skills, experiences, and creative endeavors. I am Solomon, a passionate software developer with a dedication to Web application creation.</p>
+                                
+                                <p className={`${styles.p}`}>
+                                    Throughout my journey, I have honed my skills and acquired a diverse range of experiences that have shaped me into the professional I am today. My portfolio serves as a testament to my dedication, creativity, and ability to deliver exceptional results.
+                                    In this space, you will find a collection of my best work.
+                                </p>
+                            </div>
+                            
+                            <div className={`${styles.about_Past_section_me_pic}`}>
+                                <img alt='me' />
+                            </div>
+                        </div>
+
+                        <div className={`${styles.about_Past_section_uni}`}>
+                            <div className={`${styles.about_Past_section_uni_content}`}>
+                                <div className={`${styles.line}`}>
+                                    <h3 className={`${styles.h3}`}>Bachelor of Technology in Information Technology <span>(2021 - 2025)</span></h3>
+                                </div>
+
+                                <div className={`${styles.p}`}>
+                                    I am currently pursuing my Bachelor of Technology degree in Information Technology (IT) from VIT. 
+                                    During my academic journey, I acquired a comprehensive set of skills and knowledge in various domains of IT. 
+                                </div>
+
+                                <div className={`${styles.p}`}>
+                                    <b>Web Development Project:</b><p>Developed a dynamic and responsive website using HTML, CSS, and JavaScript. 
+                                    The website showcased my skills in front-end development and included features such as user registration, 
+                                    login functionality, and a database backend.</p>
+                                </div>
+
+                                <div className={`${styles.p}`}>
+                                    <b>Database Management System:</b><p>Designed and implemented a database management system for a fictional company, ensuring efficient data organization, storage, and retrieval. 
+                                    Employed SQL queries and normalization techniques to optimize database performance.</p>
+                                </div>
+
+                                <div className={`${styles.p}`}>
+                                    <b>Information Security Project:</b><p>Conducted a comprehensive analysis of network vulnerabilities and implemented security measures to protect against potential threats. 
+                                    Explored various security frameworks, protocols, and encryption techniques to strengthen the network infrastructure.</p>
+                                </div>
+
+                            </div>
+                            <div className={`${styles.about_Past_section_uni_pic}`}>
+                                <img alt='uni pic' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${styles.buttons}`}>
+                    <div className={`${styles.buttons_x} Button`}></div>
+                    <div className={`${styles.buttons_x} Button`}></div>
+                </div> 
+            </div>                        
+        </div>
+    )
+}
--- src/components/homepagecomp/about-me.js
+++ src/components/homepagecomp/about-me.js
@@ -1,6 +1,6 @@
-import React from 'react'
 import styles from './scss/about.module.scss'
 import { useEffect, useContext, useRef, useState } from 'react'
+import Carasole from '@/components/carasole/carasole'
 import { HomeContext } from '@/contexts/HomeContext'
 import { TransitionContext } from '@/contexts/TransitionContext'
 import { gsap }  from 'gsap'
@@ -11,28 +11,27 @@ gsap.registerPlugin(ScrollTrigger)
 export default function aboutMe() {
     const { completed } = useContext(TransitionContext)
     const { MainSec } = useContext(HomeContext)
-    const backstory = useRef()
-    const [backstoryState, setbackstoryState] = useState(false)
+    // const backstory = useRef()
+    // const [backstoryState, setbackstoryState] = useState(false)
 
-    function handleClickAbout(){
-        setbackstoryState(true)
-    }
+    // function handleClickAbout(){
+    //     setbackstoryState(true)
+    // }
 
-    function handleClickCross(){
-        setbackstoryState(false)
-    }
+    // function handleClickCross(){
+    //     setbackstoryState(false)
+    // }
 
-    useEffect(()=>{
-        console.log(backstoryState)
-        const el = backstory.current.querySelector('.Backstory')
+    // useEffect(()=>{
+    //     console.log(backstoryState)
+    //     const el = backstory.current.querySelector('.Backstory')
+    //     if(backstoryState){
+    //         gsap.to(el,{x: 0, opacity: 1})
+    //     }else{
+    //         gsap.to(el,{x: '100dvw', opacity: 0})
+    //     }
 
-        if(backstoryState){
-            gsap.to(el,{xPercent: -100, opacity: 1})
-        }else{
-            gsap.to(el,{xPercent: 100, opacity: 0})
-        }
-
-    },[backstoryState])
+    // },[backstoryState])
 
     useEffect(() =>{
         if (!completed.state) return
@@ -82,32 +81,9 @@ export default function aboutMe() {
                     <span className={styles.span}>creating beautiful web designs</span>
                 </div>
             </div>
-            <div className={`${styles.about}`} ref={backstory}>
-                <button className={`${styles.about_word}`} onClick={handleClickAbout}>
-                    ABOUT ME
-                </button>
-                
-                <div className={`${styles.about_Past} Backstory`}>
-                    <div className={`${styles.about_Past_cross}`} onClick={handleClickCross}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="50px" height="50px" viewBox="0 0 32 32">
-                            <path d="M18.8,16l5.5-5.5c0.8-0.8,0.8-2,0-2.8l0,0C24,7.3,23.5,7,23,7c-0.5,0-1,0.2-1.4,0.6L16,13.2l-5.5-5.5  c-0.8-0.8-2.1-0.8-2.8,0C7.3,8,7,8.5,7,9.1s0.2,1,0.6,1.4l5.5,5.5l-5.5,5.5C7.3,21.9,7,22.4,7,23c0,0.5,0.2,1,0.6,1.4  C8,24.8,8.5,25,9,25c0.5,0,1-0.2,1.4-0.6l5.5-5.5l5.5,5.5c0.8,0.8,2.1,0.8,2.8,0c0.8-0.8,0.8-2.1,0-2.8L18.8,16z"/>
-                        </svg>
-                    </div>
-                    Welcome to my portfolio page! Here, you will discover a showcase of my skills, experiences, and creative endeavors. I am [Your Name], a passionate [profession/field] with a dedication to [describe your area of expertise or interest].
 
-                    Throughout my journey, I have honed my skills and acquired a diverse range of experiences that have shaped me into the professional I am today. My portfolio serves as a testament to my dedication, creativity, and ability to deliver exceptional results.
+            <Carasole />
 
-                    In this space, you will find a collection of my best work, demonstrating my proficiency in [list key skills or areas of expertise]. From [mention specific projects, achievements, or collaborations], each piece embodies my unwavering commitment to quality and innovation.
-
-                    I believe that every project presents an opportunity for growth and learning, and I approach each endeavor with an open mind, a passion for excellence, and a determination to exceed expectations. With a keen eye for detail and a strong understanding of [mention relevant tools or technologies], I strive to deliver impactful solutions that leave a lasting impression.
-
-                    Feel free to explore my portfolio and immerse yourself in the world of my creations. Whether you are a potential client, collaborator, or simply someone interested in my work, I hope you find inspiration and discover the unique perspective I bring to each project.
-
-                    Thank you for visiting, and I look forward to connecting with you soon to discuss how I can contribute to your next venture.
-                </div>
-                    
-            </div>
-            
             <div className={`${styles.waves} ${styles.wave0}`}></div>
             <div className={`${styles.waves} ${styles.wave1}`}></div>
             <div className={`${styles.waves} ${styles.wave2}`}></div>
